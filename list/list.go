724a61928e48500c075e69576948a863e1d3ff29
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-19 20:00:18 -0500

    Updated the comment of Print to show the time complexity of the operation

--- list/list.go
+++ list/list.go
@@ -15,6 +15,9 @@ type Node[T interface{}] struct {
 
 /*
 Print - Iterate through each element of the linked list and print the data that it is storing
+
+Time Complexity: o(N) where N is the length of the list
+Reason: Each node must be iterated over in order to print the data stored in the node.
 */
 func (list *Node[T]) Print() {
 	nodeCopy := list
