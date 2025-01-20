5104fa8e7d8332828d6cd4bd99fe3043ba67a048
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-19 20:02:27 -0500

    Implemented a test case for testing the output of the Print() function

--- test/list_test.go
+++ test/list_test.go
@@ -1,1 +1,16 @@
 package test
+
+import (
+	"github.com/stevezaluk/go-collections/list"
+	"testing"
+)
+
+/*
+TestListPrint - Test the Print() function of the linked list
+*/
+func TestListPrint(t *testing.T) {
+	head := &list.Node[string]{Data: "headNode"}
+	head.Next = &list.Node[string]{Data: "secondNode"}
+
+	head.Print()
+}
