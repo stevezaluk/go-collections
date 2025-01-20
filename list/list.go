d926505444848e36d8c56da89c457f9a8bcd59c4
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-19 19:59:16 -0500

    Implemented a function to print the data stored in each node

--- list/list.go
+++ list/list.go
@@ -1,5 +1,7 @@
 package list
 
+import "fmt"
+
 /*
 Node - A representation of a linear Doubly Linked List
 */
@@ -9,4 +11,17 @@ type Node[T interface{}] struct {
 	Prev  *Node[T]
 
 	Data T
+}
+
+/*
+Print - Iterate through each element of the linked list and print the data that it is storing
+*/
+func (list *Node[T]) Print() {
+	nodeCopy := list
+
+	for nodeCopy != nil {
+		fmt.Println(nodeCopy.Data)
+
+		nodeCopy = nodeCopy.Next
+	}
 }
