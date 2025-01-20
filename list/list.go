d183cc660a957f7eeb9801ebf6552fffd410cfa2
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-19 20:21:42 -0500

    Create a new Structure called LinkedList to track the head and tail of the list. This allows us to append at O(1) time complexity

--- list/list.go
+++ list/list.go
@@ -3,7 +3,17 @@ package list
 import "fmt"
 
 /*
-Node - A representation of a linear Doubly Linked List
+LinkedList - A representation of a linear Doubly Linked List
+*/
+type LinkedList[T any] struct {
+	Head *Node[T]
+	Tail *Node[T]
+
+	Length int
+}
+
+/*
+Node - A representation of a single node in the linked list
 */
 type Node[T any] struct {
 	Index int
@@ -19,8 +29,8 @@ Print - Iterate through each element of the linked list and print the data that 
 Time Complexity: o(N) where N is the length of the list
 Reason: Each node must be iterated over in order to print the data stored in the node.
 */
-func (list *Node[T]) Print() {
-	nodeCopy := list
+func (list *LinkedList[T]) Print() {
+	nodeCopy := list.Head
 
 	for nodeCopy != nil {
 		fmt.Println(nodeCopy.Data)
