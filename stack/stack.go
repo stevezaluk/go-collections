2179193cc99311ba4b3245d2e63e96cbc1b2111a
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-21 11:34:17 -0500

    Implemented basic function for push data to the top of the stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,6 +1,19 @@
 package stack
 
+/*
+Stack - Represents a LIFO stack data structure. We use an array internally to store the data as this provides us
+with a O(1) time when randomly accessing elements
+*/
 type Stack[T comparable] struct {
 	Data   []T
 	Length int
+}
+
+/*
+Push - Push a new element to the top of the stack
+*/
+func (stack *Stack[T]) Push(data T) {
+	stack.Data = append(stack.Data, data)
+
+	stack.Length += 1
 }
