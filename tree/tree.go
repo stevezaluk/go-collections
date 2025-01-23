93edd8b10aa0e319acfab6e87f9570d786ad2ffb
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 09:10:37 -0500

    Defined the basic structure of the binary tree

--- tree/tree.go
+++ tree/tree.go
@@ -1,1 +1,24 @@
 package tree
+
+/*
+BinaryTree - Represents an ordered binary tree
+*/
+type BinaryTree struct {
+	Head *BinaryNode
+}
+
+/*
+BinaryNode - Represents one node in the binary tree
+*/
+type BinaryNode struct {
+	Data  int
+	Left  *BinaryNode
+	Right *BinaryNode
+}
+
+/*
+New - Initialize a new binary tree and set the head node to the data passed in the parameter
+*/
+func New(data int) *BinaryTree {
+	return &BinaryTree{Head: &BinaryNode{Data: data}}
+}
