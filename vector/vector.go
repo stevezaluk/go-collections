cc5ad3efc5f7cc5e47f0ac23306c317fc3e2f0d1
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-30 17:19:39 -0500

    Defined the vector structure and its interface

--- vector/vector.go
+++ vector/vector.go
@@ -1,3 +1,23 @@
 package vector
 
+/*
+IVector - An interface describing all the functions that the Vector implements.
+It accepts an interface as its generic type, allowing you to store any
+data type needed
+*/
+type IVector[T interface{}] interface {
+	Append(T)
+	Get(int) interface{}
+	Insert(T, int)
+	Remove(int)
+	Length() int
+}
 
+/*
+Vector - A representation of a vector (or a dynamic array). Its underlying array
+is allocated with 0 elements
+*/
+type Vector[T interface{}] struct {
+	data   [0]T
+	length int
+}
