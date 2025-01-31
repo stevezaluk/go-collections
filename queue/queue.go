c92adb66f5dc627263177965a4ee1cc7a843fdb2
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-31 13:45:43 -0500

    Defined the structure and interface for Queue

--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,22 @@
 package queue
+
+import "github.com/stevezaluk/go-collections/list"
+
+/*
+IQueue - An interface describing all the functions that a queue implements. Implementation is
+implicit in go, so no need to explicitly declare this
+*/
+type IQueue[T comparable] interface {
+	Enqueue(T)
+	Dequeue() T
+	Front() T
+	IsEmpty() bool
+	Size() int
+}
+
+/*
+Queue - Represents a basic first-in-first-out (FIFO) queue
+*/
+type Queue[T comparable] struct {
+	data *list.LinkedList[T]
+}
