0542f0aab2282a6aba1597b7d08dc776b69d7997
Steven A. Zaluk <stevezaluk@gmail.com>
2025-02-01 23:55:15 -0500

    Removed the second generic from the KeyPair and HashMap declerations and replaced it with a string

--- hash/hashmap.go
+++ hash/hashmap.go
@@ -3,32 +3,33 @@ package hash
 /*
 KeyPair - A structure representing a key pair in a Hash Map
 */
-type KeyPair[T comparable, K any] struct {
-	Key   T
-	Value K
+type KeyPair[T any] struct {
+	Key   string
+	Value T
 }
 
 /*
 NewKeyPair - A constructor for the KeyPair structure. Returns a pointer
 to the KeyPair initialized with the values that are provided as arguments
 */
-func NewKeyPair[T comparable, K any](key T, value K) *KeyPair[T, K] {
-	return &KeyPair[T, K]{Key: key, Value: value}
+func NewKeyPair[T any](key string, value T) *KeyPair[T] {
+	return &KeyPair[T]{Key: key, Value: value}
 }
 
 /*
 HashMap - Represents the HashMap as a whole and encapsulates it's all of its
 logic.
 */
-type HashMap[T comparable, K any] struct {
-	data   []*KeyPair[T, K]
-	length uint64
+type HashMap[T any] struct {
+	data     []*KeyPair[T]
+	length   uint64
+	capacity uint64
 }
 
 /*
 NewHashMap - A constructor for the HashMap structure. Returns a pointer
 to the HashMap structure with
 */
-func NewHashMap[T comparable, K any]() *HashMap[T, K] {
-	return &HashMap[T, K]{data: make([]*KeyPair[T, K], 0), length: 0}
+func NewHashMap[T string, K any]() *HashMap[T] {
+	return &HashMap[T]{data: make([]*KeyPair[T], 0), length: 0, capacity: 0}
 }
